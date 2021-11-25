6cdb73b7ef6941b7078945f76306ccd1303d55fa
toBeOfUse <mitchjacov@gmail.com>
2021-11-25 00:17:55 -0500

    change lighting and add anti-aliasing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import * as THREE from "three";
-import { MeshPhongMaterial, Raycaster } from "three";
+import { Mesh, MeshStandardMaterial, Raycaster } from "three";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader";
 import { loadGLTF, CornControls } from "./utilities";
 
 function animate(renderFunction: () => void) {
@@ -13,7 +17,7 @@ async function createScene() {
   const aspectRatio = window.innerWidth / window.innerHeight;
   const camera = new THREE.PerspectiveCamera(vFOV, aspectRatio, 0.1, 1000);
 
-  // set up renderer
+  // set up renderer and composer
   const renderer = new THREE.WebGLRenderer({ alpha: true });
   renderer.toneMapping = THREE.LinearToneMapping;
   renderer.toneMappingExposure = 0.9;
@@ -21,9 +25,25 @@ async function createScene() {
   renderer.domElement.style.backgroundImage = "url(fieldbg.jpg)";
   renderer.domElement.style.backgroundSize = "cover";
   renderer.domElement.style.backgroundPosition = "center";
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  const scene = new THREE.Scene();
+
+  const fxaaPass = new ShaderPass(FXAAShader);
+  const composer = new EffectComposer(renderer);
+  const renderPass = new RenderPass(scene, camera);
+  composer.addPass(renderPass);
+  composer.addPass(fxaaPass);
+  composer.setPixelRatio(window.devicePixelRatio);
 
   function initializeDimensions() {
     renderer.setSize(window.innerWidth, window.innerHeight);
+    const pixelRatio = renderer.getPixelRatio();
+    fxaaPass.material.uniforms["resolution"].value.x =
+      1 / (window.innerWidth * pixelRatio);
+    fxaaPass.material.uniforms["resolution"].value.y =
+      1 / (window.innerHeight * pixelRatio);
+    composer.setSize(window.innerWidth, window.innerHeight);
     const aspectRatio = window.innerWidth / window.innerHeight;
     camera.position.z = 4;
     if (aspectRatio < 4 / 3) {
@@ -36,18 +56,12 @@ async function createScene() {
   initializeDimensions();
   window.addEventListener("resize", initializeDimensions);
 
-  // create scene and add objects and lights
-  const scene = new THREE.Scene();
+  // initialize scene and add objects and lights
   const corn = (await loadGLTF("/corn.glb")).scene;
-  const kernelMaterial = new MeshPhongMaterial({
-    color: 0xfff159,
-    shininess: 1,
-  });
-  for (const child of corn.children) {
-    if (child.name.includes("Kernel") && child instanceof THREE.Mesh) {
-      child.material = kernelMaterial;
-    }
-  }
+  (
+    (corn.children.find((c) => c.name == "Cob") as Mesh)
+      .material as MeshStandardMaterial
+  ).roughness = 1;
   scene.add(corn);
 
   function getKernelCount() {
@@ -55,14 +69,11 @@ async function createScene() {
   }
   const initialKernelCount = getKernelCount();
 
-  const ambient = new THREE.AmbientLight(0xffffff, 0.3);
+  const ambient = new THREE.AmbientLight(0xffffff, 0.5);
   scene.add(ambient);
-  const point = new THREE.PointLight(0xffffff, 0.5, 100);
-  point.position.set(0, -10, 0);
+  const point = new THREE.PointLight(0xffffff, 1.1, 100);
+  point.position.set(0, 20, 20);
   scene.add(point);
-  const point2 = new THREE.PointLight(0xffffff, 0.8, 100);
-  point2.position.set(0, 5, 5);
-  scene.add(point2);
 
   // set up controls
   const controls = new CornControls(
@@ -95,7 +106,7 @@ async function createScene() {
   // create render function that utilizes the renderer, scene, camera, and controls
   const renderFunction = () => {
     controls.applyRotation(corn);
-    renderer.render(scene, camera);
+    composer.render();
     document.querySelector("#info").innerHTML = `${(
       ((initialKernelCount - getKernelCount()) / initialKernelCount) *
       100
