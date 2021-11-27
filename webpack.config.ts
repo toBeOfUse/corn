7f8dc19bb5f1c4aa8a1d37ec6ff025e70d17a01b
toBeOfUse <mitchjacov@gmail.com>
2021-11-26 20:44:08 -0500

    enable source maps in development mode

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,14 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from "path";
-export default {
-  mode: "development",
+export default (_, options) => ({
+  mode: options.mode || "development",
   entry: "./src/index.ts",
   output: {
     path: path.join(__dirname, "dist"),
     filename: "index_bundle.[contenthash].js",
   },
+  devtool:
+    !options.mode || options.mode == "development" ? "eval-source-map" : false,
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
   },
@@ -14,4 +16,4 @@ export default {
     rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
   },
   plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
-};
+});
